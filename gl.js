9130f81b5a7908fc1d52a285ee51baadaa25fb73
daquino <daquino@fly.thruhere.net>
2010-11-06 19:28:51 +0000

    cleaned up gl.js a bit; added a table to save current perspective settings; added a model view matrix stack with push/pop;

--- gl.js
+++ gl.js
@@ -1,20 +1,25 @@
 var GL = function(canvas)
 {
 	try { this.gl = canvas.getContext("experimental-webgl"); } 
-		catch(e) { log(e); }
+		catch(e) { return log(e); }
+
 	if (!this.gl) 
 		{ return log("Could not initialise WebGL, sorry :-("); }
-	this.loadIdentity();
-	this.perspective(70,1.0,10.0,10000.0);
-	this.__noSuchMethod__ = function(id,args){
-	       alert("no: "+id); 
-	}
+
+	this.__noSuchMethod__ = function(id,args)
+		{ alert("no: "+id); }
+
+	this.current_perspective['aspect'] = canvas.width/canvas.height;
+	this.set_perspective();
+
+	this.gl.viewport(0,0,canvas.width,canvas.height);
 }
 GL.prototype = 
 {
-	mvMatrix: null,
-	pMatrix: null,
-	gl: null,
+	mvMatrix:      null,
+	mvMatrixStack: [],
+	pMatrix:       null,
+	gl:            null,
 	scale: function(x,y,z)
 	{
 		this.multMatrix($M([
@@ -36,18 +41,60 @@ GL.prototype =
 	},
 	multMatrix: function(m) 
 	{ 
-		this.mvMatrix = mvMatrix.x(m); 
+		this.mvMatrix = this.mvMatrix.x(m); 
 	},
 	loatMatrix: function(m)
 	{
 		this.mvMatrix = m;
+	},
+	pushMatrix: function(m)
+	{
+		if(m)
+		{
+			this.mvMatrixStack.push(m.dup());
+			this.mvMatrix = m.dup();
+		}
+		else
+		{
+			this.mvMatrixStack.push(this.mvMatrix.dup);
+		}
+	},
+	popMatrix: function()
+	{
+		if(this.mvMatrixStack.length == 0)
+		{
+			throw "Invalid popMatrix!";
+		}
+		this.mvMatrix = this.mvMatrixStack.pop();
+		return this.mvMatrix;
 	},
 	translate: function(v) 
 	{
 		var m = Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4();
 		this.multMatrix(m);
 	},
-	perspective: function (fovy, aspect, znear, zfar) {
+	current_perspective:
+	{
+		fovy:   70, 
+		aspect: 1.0, 
+		znear:  0.1, // 10.0, 
+		zfar:   10000.0 
+	},
+	set_perspective: function()
+	{
+		this.perspective(
+			this.current_perspective['fovy'],
+			this.current_perspective['aspect'],
+			this.current_perspective['znear'],
+			this.current_perspective['zfar']
+		);
+	},
+	perspective: function (fovy, aspect, znear, zfar)
+	{
+		this.current_perspective['fovy']   = fovy;
+		this.current_perspective['aspect'] = aspect;
+		this.current_perspective['znear']  = znear;
+		this.current_perspective['zfar']   = zfar;
 		this.pMatrix = makePerspective(fovy, aspect, znear, zfar);
 	}
 }

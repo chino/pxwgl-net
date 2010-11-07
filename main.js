62d27fca7fbbc4302a7f01f1e065d1f18a513db6
daquino <daquino@fly.thruhere.net>
2010-11-07 05:52:04 +0000

    moved things around; added networking ability; updated to use rest of updates;

--- main.js
+++ main.js
@@ -118,6 +118,7 @@
 	{	
 		init: function()
 		{
+			Gamma.build(1.0);
 			textures.preload();
 //			textures.load_example();
 		},
@@ -145,20 +146,14 @@
 
 	var setMatrixUniforms = function() 
 	{
-		gl.uniformMatrix4fv(
-			shaderProgram.pMatrixUniform, 
-			false, 
-			new Float32Array(_gl.pMatrix.flatten())
-		);
-		gl.uniformMatrix4fv(
-			shaderProgram.mvMatrixUniform, 
-			false, 
-			new Float32Array(_gl.mvMatrix.flatten())
-		);
+		gl.uniformMatrix4fv( shaderProgram.pMatrixUniform, false, _gl.pMatrixPacked );
+		gl.uniformMatrix4fv( shaderProgram.mvMatrixUniform, false, _gl.mvMatrixPacked );
 	}
 
 	var init_buffers = function()
 	{
+		// init level vertex/color/tcords
+
 		triangleVertexPositionBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(level.vertices), gl.STATIC_DRAW);
@@ -176,10 +171,94 @@
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(level.tcords), gl.STATIC_DRAW);
 		triangleVertexTCordBuffer.itemSize = 2;
 		triangleVertexTCordBuffer.numItems = level.tcords.length/2;
+
+		// init pyramid for multiplayer rendering
+
+	  pyramidVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
+    var vertices = [
+        // Front face
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+         0.0,  1.0,  0.0,
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+         0.0,  1.0,  0.0,
+        // Back face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         0.0,  1.0,  0.0,
+        // Left face
+        -1.0, -1.0,  1.0,
+        -1.0, -1.0, -1.0,
+         0.0,  1.0,  0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    pyramidVertexPositionBuffer.itemSize = 3;
+    pyramidVertexPositionBuffer.numItems = 12;
+
+
+    pyramidVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
+    var colors = [
+        // Front face
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        // Right face
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        // Back face
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        // Left face
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0, 1.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    pyramidVertexColorBuffer.itemSize = 4;
+    pyramidVertexColorBuffer.numItems = 12;
 	}
 
-	var init_info = function()
+	var init_gl = function()
 	{
+		canvas = document.getElementById("lesson02-canvas");
+		_gl = new GL(canvas);
+		gl = _gl.gl;
+
+		init_shaders()
+		init_buffers();
+
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+		gl.depthFunc(gl.LESS);
+		gl.enable(gl.CULL_FACE);
+		gl.frontFace(gl.CW);
+		if(gl.PERSPECTIVE_CORRECTION_HINT)
+			{gl.hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST);}
+	}
+
+	var init_camera = function()
+	{
+		camera = new View();
+/*
+		camera.pos.x = -2004;
+		camera.pos.y = -561;
+		camera.pos.z = -4020;
+		camera.rotate(90,0,0)
+*/
+	}
+
+	var init_ui = function()
+	{
+
+		// info pain
 		var verts = level.vertices.length/3;
 		$('#level-verts').html(verts);
 		$('#level-triangles').html(verts/3);
@@ -193,55 +272,11 @@
 			unique[vert] = true;
 		}
 		$('#level-verts-unique').html(count);
-	}
 
-	current_far = 10000.0;
-	current_fovy = 70.0;
+		// reset all changeable fields
+		$('form')[0].reset();
 
-	var init_gl = function()
-	{
-		canvas = document.getElementById("lesson02-canvas");
-		_gl = new GL(canvas);
-		gl = _gl.gl;
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clearDepth(1.0);
-		gl.enable(gl.DEPTH_TEST);
-		gl.depthFunc(gl.LESS);
-		gl.enable(gl.CULL_FACE);
-		gl.frontFace(gl.CW);
-		if(gl.PERSPECTIVE_CORRECTION_HINT)
-			{gl.hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST);}
-		_gl.perspective(current_fovy, 1.0, 10.0, current_far);
-	}
-
-	var init_camera = function()
-	{
-		camera = new View();
-		camera.pos.x = -2004;
-		camera.pos.y = -561;
-		camera.pos.z = 4020;
-		camera.rotate(-90,0,0);
-	}
-
-	var init_inputs = function()
-	{
-		mouse = new Mouse(canvas);
-		var set = function()
-		{
-			document.onkeydown = keyboard.press;
-			document.onkeyup = keyboard.release;
-		}
-		var unset = function()
-		{
-			document.onkeydown = null;
-			document.onkeyup = null;
-		}
-		canvas.onmouseover = set;
-		canvas.onmouseout = unset;
-	}
-
-	var init_ui = function()
-	{
+		// shader controls
 		$('#vertex-shader-apply').click(reinit_shaders);
 		$('#vertex-shader-reset').click(function(){
 			$.get( "vertex_shader.glsl", function(str) { 
@@ -249,7 +284,6 @@
 				reinit_shaders();
 			});
 		});
-
 		$('#fragment-shader-apply').click(reinit_shaders);
 		$('#fragment-shader-reset').click(function(){
 			$.get( "fragment_shader.glsl", function(str) { 
@@ -258,21 +292,25 @@
 			});
 		});
 
-		mouse_accell = 3;
-		$('#mouse-accell').keyup(function()
-		{
-			mouse_accell = this.value;
-		});
+		// form fields
+
+		mouse_accell = $('#mouse-accell').val();
+		$('#mouse-accell').keyup(function() { mouse_accell = this.value; });
+
+		$('#fovy').val(_gl.current_perspective['fovy']);
 		$('#fovy').keyup(function()
 		{
-			current_fovy = this.value;
-			_gl.perspective(this.value, 1.0, 10.0, current_far);
+			_gl.current_perspective['fovy'] = this.value;
+			_gl.set_perspective();
 		});
+
+		$('#far').val(_gl.current_perspective['zfar']);
 		$('#far').keyup(function()
 		{
-			current_far = this.value;
-			_gl.perspective(current_fovy, 1.0, 10.0, this.value);
+			_gl.current_perspective['zfar'] = parseInt(this.value);
+			_gl.set_perspective();
 		});
+
 		$('#fog').click(function()
 		{
 			gl.uniform1i(shaderProgram.enableFogUniform, this.checked);
@@ -303,9 +341,9 @@
 		});
 		render_mode = gl.TRIANGLES;
 		$('#rendermode-dropdown').change(function()
-                {
+		{
 			render_mode = gl[this.value.toUpperCase()];
-                });
+		});
 		$('#gamma-value').keyup(function()
 		{
 			Gamma.build(this.value);
@@ -326,10 +364,14 @@
 		$('#canvas-width').keyup(function()
 		{
 			canvas.width = this.value;
+			_gl.set_perspective();
+			_gl.set_viewport();
 		});
 		$('#canvas-height').keyup(function()
 		{
 			canvas.height = this.value;
+			_gl.set_perspective();
+			_gl.set_viewport();
 		});
 		$("#gamma-canvas").click(function(e)
 		{
@@ -361,63 +403,163 @@
 	{
 		if(!drawOK){ return; };
 
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		// set global uniforms and gl properties
+		gl.uniform1i(shaderProgram.timeUniform, (new Date()).getTime());
+		gl.activeTexture(gl.TEXTURE0);
+		gl.uniform1i(shaderProgram.samplerUniform, 0);
 
+		// clear the screen
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		// move and look around the seen
 		var xy = mouse.get();
-		camera.rotate(xy.x*mouse_accell,xy.y*mouse_accell,0);
+		camera.rotate(-xy.x*mouse_accell,-xy.y*mouse_accell,0);
 		camera.move(movement);
 		camera.place_camera();
+		setMatrixUniforms();
 
+		// render the level
 		if(shaderProgram.vertexPositionAttribute >= 0)
 		{
 			gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
 			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
 				triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		}
-
 		if(shaderProgram.vertexColorAttribute >= 0)
 		{
 			gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
 			gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
 				triangleVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		}
-
 		if(shaderProgram.vertexCoordAttribute >= 0)
 		{
 			gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexTCordBuffer);
 			gl.vertexAttribPointer(shaderProgram.vertexCoordAttribute,
 				triangleVertexTCordBuffer.itemSize, gl.FLOAT, false, 0, 0);
 		}
-
-		setMatrixUniforms();
-		gl.uniform1i(shaderProgram.timeUniform, (new Date()).getTime());
-		gl.activeTexture(gl.TEXTURE0);
-		gl.uniform1i(shaderProgram.samplerUniform, 0);
-
 		for(var i=0; i<level.indices.length; i++)
 		{
 			gl.bindTexture(gl.TEXTURE_2D, Images.get(level.indices[i][2]).texture);
 			gl.drawArrays(render_mode, level.indices[i][0], level.indices[i][1]);
 		}
 
+		// set pyramid buffers
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+			pyramidVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+			pyramidVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+		// render players
+		for(var p in net.players)
+		{
+			var player = net.players[p];
+			var up = player.dir.vector("up");
+			var right = player.dir.vector("right");
+			var forward = player.dir.vector("forward");
+			_gl.pushMatrix();
+			_gl.multMatrix($M([
+	      [right.x, right.y, right.z, 0.0],
+ 		    [up.x, up.y, up.z, 0.0],
+    	  [forward.x, forward.y, forward.z, 0.0],
+      	[player.pos.x, player.pos.y, player.pos.z, 1.0]
+	    ]).transpose())
+			setMatrixUniforms();
+			gl.drawArrays(gl.TRIANGLES, 0, pyramidVertexPositionBuffer.numItems);
+			_gl.popMatrix();
+		}
+
 		// update info pain
-		$('#fps').html(fps.run());
 		$('#pos').html(camera.pos.to_s());
 		$('#quat').html(camera.orientation.to_s());
 		$('#mouse').html(xy.to_s());
+		$('#players').html(net.player_count);
+		$('#fps').html(fps.run());
+	}
+
+	var net = 
+	{
+		socket: null,
+		players: {},
+		player_count: 0,
+		connect: function()
+		{
+			net.socket = new WebSocket("ws://fly.thruhere.net:8080");
+			net.socket.onopen = net.onopen;
+			net.socket.onclose = net.onclose;
+			net.socket.onmessage = net.receive;
+		},
+		onopen: function()
+		{
+			log("connected to server"); 
+			net.interval = window.setInterval( net.send_update, 1000/10 );
+		},
+		onclose: function()
+		{ 
+			log("disconnected from server"); 
+			window.clearInterval( net.interval );
+			net.interval = null;
+			net.players = [];
+		},
+		receive: function(msg)
+		{
+			//log("got a message: "+msg.data);
+			try 
+			{
+				var packet = eval("("+msg.data+")");
+				if(!net.players[packet.id]) // new player
+				{
+					net.players[packet.id] = { id: packet.id }
+					log("new player with id: "+packet.id);
+					net.player_count++;
+				}
+				var player = net.players[packet.id];
+				player.pos = new Vec(
+					packet.data.pos[0],
+					packet.data.pos[1],
+					packet.data.pos[2]
+				);
+				player.dir = new Quat(
+					packet.data.dir[0],
+					packet.data.dir[1],
+					packet.data.dir[2],
+					packet.data.dir[3]
+				);
+			}
+			catch (e)
+			{ 
+				log("error "+e+" while trying to eval message: "+msg);
+			}
+		},
+		send: function(msg)
+		{
+			if(!msg){return;}
+			try { net.socket.send(msg); }
+			catch (e) { log("error ("+e+") attempting to send message: "+msg); return; }
+			//log("sent message: "+msg);
+		},
+		close: function()
+		{
+			net.socket.close();
+		},
+		send_update: function()
+		{
+			net.send(
+				"{ pos: ["+camera.pos.to_s()+"], "+
+				"dir: ["+camera.orientation.to_s()+"] }"
+			);
+		}
 	}
 
 	$(document).ready(function() 
 	{
-		Gamma.build(1.0); // build default table that reflects no change
-		init_info();
 		init_gl();
 		init_ui();
-		init_shaders()
-		init_buffers();
 		init_camera();
 		init_inputs();
+		net.connect();
 		textures.init();
-		setInterval(draw, 1);
+		draw_interval = setInterval(draw, 1);
 	});
-

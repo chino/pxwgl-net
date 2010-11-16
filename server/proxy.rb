42d08e65b21dfa80153ed175f8428f169e4d1b81
Daniel Aquino <mr.danielaquino@gmail.com>
2010-11-15 21:09:55 -0500

    updated to event machine based server; added a load balancer that can sorta work to detect web socket connects and properly proxy to the web-socket server or if not to the normal http server

--- server/proxy.rb
+++ server/proxy.rb
@@ -1,34 +1,23 @@
 #!/usr/bin/env ruby
-require "./lib/web-socket-ruby/lib/web_socket.rb"
-def die msg="I died"
-	puts msg
-	exit 1
-end
-def usage
-	die "Usage: #{$0} <host> <port> <domain>"
-end
-usage if ARGV.length < 3
-server = WebSocketServer.new(
-	:host => ARGV[0],
-	:port => ARGV[1],
-	:accepted_domains => [ARGV[3]]
-)
+require "rubygems"
+require "em-websocket"
 id=0
 connections = []
-server.run() do |connection|
-	begin
-		my_id = (id+=1)
-		connections << connection
-		puts "new client, id=#{my_id}, uri=#{connection.path}"
-		connection.handshake
-		while data = connection.receive
-			puts "#{my_id} = #{data}"
-			connections.each do |c|
-				next if c == connection
-				c.send "{ id: #{my_id}, data: #{data} }"
-			end
-		end	
-	ensure
-		connections.delete connection
-	end
+EventMachine::WebSocket.start(:host => "localhost", :port => ARGV[0]) do |ws|
+	my_id = (id+=1)
+	ws.onopen {
+		connections << ws
+		puts "new client, id=#{my_id}"
+	}
+	ws.onclose {
+		puts "lost connection"
+		connections.delete ws
+	}
+	ws.onmessage {|data|
+		puts "#{my_id} = #{data}"
+		connections.each do |connection|
+			next if connection == ws
+			connection.send "{ id: #{my_id}, data: #{data} }"
+		end
+	}
 end
